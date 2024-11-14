b35c1cdbf421acaefd5e35430fc74cc482b9de50
Benn <bennkiche7@gmail.com>
2024-11-14 22:48:58 +0300

    src/components/Contacts.jsx

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,14 +1,28 @@
 import React from "react";
-function Contacts(){
+
+function Contacts() {
     return (
         <>
-        <h1>Contact Us</h1>
-        <ul className="contacts">
-            <a href="">0111489908</a>
-            <a href="">Facebook</a>
-            <a href="">Whatsapp</a>
-        </ul>
+            <h1>Contact Us</h1>
+            <ul className="contacts">
+                <a href="tel:0111489908">0111489908</a>
+                <a 
+                    href="https://www.facebook.com/profile.php?id=100080886662937" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                >
+                    Facebook
+                </a>
+                <a 
+                    href="https://wa.me/254111489908" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                >
+                    WhatsApp
+                </a>
+            </ul>
         </>
-    )
+    );
 }
-export default Contacts
+
+export default Contacts;
